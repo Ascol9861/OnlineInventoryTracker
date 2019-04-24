dd21bc930da069c63937a67d91959db95635c214
Ascol9861 <ascolparajuli65@gmail.com>
2019-04-24 17:40:13 +0545

    Login and register layout made

--- app/src/main/java/com/example/onlineinventorytracker/MainActivity.java
+++ app/src/main/java/com/example/onlineinventorytracker/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.onlineinventorytracker;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import fragments.LoginFragment;
+import fragments.RegisterFragment;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnLogin;
+    private Boolean status = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(status){
+            LoginFragment loginFragment = new LoginFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer, loginFragment);
+            fragmentTransaction.commit();
+            btnLogin.setText("Load Register Fragment");
+            status = false;
+        }
+        else {
+            RegisterFragment registerFragment = new RegisterFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer, registerFragment);
+            fragmentTransaction.commit();
+            btnLogin.setText("Login Page here");
+            status = true;
+        }
     }
 }
